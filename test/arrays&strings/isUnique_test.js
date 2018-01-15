29ebf31b24c5e6a8adf474a123d18fc6e1648bd2
Jimmy <jdicolandrea@gmail.com>
2018-01-15 16:06:15 -0500

    add basic testing ability

---
+++ test/arrays&strings/isUnique_test.js
@@ -0,0 +1,14 @@
+var chai = require('chai');
+let assert = chai.assert
+let expect = chai.expect
+let should = chai.should()
+let string;
+
+describe('String', function() {
+  
+  it('should be empty', function() {
+    string = ""
+    assert.equal(string.length, 0)
+  })
+}
+)
