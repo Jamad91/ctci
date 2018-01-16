aa9b88d7709576b83c0f44d467a08e2e7fa690c5
Jimmy <jdicolandrea@gmail.com>
2018-01-16 01:47:01 -0500

    add isUnique with testing

--- problems/arrays&strings/isUnique.js
+++ problems/arrays&strings/isUnique.js
@@ -1,3 +1,18 @@
 // Implement an algorithm to determine if a string has all unique characters. 
 // What if you cannot use additional structures?
 
+function isUnique(str) {
+  let obj = {}
+  for (let i = 0; i < str.length; i++) {
+    if(!obj[str[i]]) {
+      obj[str[i]] = true 
+    } else {
+      return false
+    }
+  }
+  return true
+}
+
+if(typeof exports !== 'undefined') {
+    module.exports.isUnique = isUnique;
+}
--- test/arrays&strings/isUnique_test.js
+++ test/arrays&strings/isUnique_test.js
@@ -2,13 +2,28 @@ var chai = require('chai');
 let assert = chai.assert
 let expect = chai.expect
 let should = chai.should()
+let isUnique = require('../../problems/arrays&strings/isUnique.js').isUnique
 let string;
 
-describe('String', function() {
+describe('isUnique', function() {
   
-  it('should be empty', function() {
-    string = ""
-    assert.equal(string.length, 0)
+  it('is a function', function() {
+    expect(typeof isUnique === 'function').equal(true)
   })
-}
-)
+  
+  it('returns a bool', function() {
+    string = "goo"
+    expect(typeof(isUnique(string)) == 'boolean').equal(true)
+  })
+  
+  it('returns false when string does not contain unique characters', function() {
+    string = "goo"
+    expect(isUnique(string)).equal(false)
+  })
+  
+  it('returns true when string contains only unique characters', function() {
+    string = "abcd"
+    expect(isUnique(string)).equal(true)
+  })
+  
+})
