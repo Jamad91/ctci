44a49ab3f68e0bcf681b1b86a11b403f4857d17b
Jimmy <jdicolandrea@gmail.com>
2018-01-17 21:52:35 -0500

    zeroMatrix method and tests

--- problems/arrays&strings/07_zeroMatrix.js
+++ problems/arrays&strings/07_zeroMatrix.js
@@ -1,7 +1,28 @@
 // Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0
 
-function zeroMatrix(str) {
-  
+function zeroMatrix(arr) {
+  let zero
+  for(let i = 0; i < arr.length; i++) {
+    zero = [i]
+    for(let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === 0) {
+        zero.push(j)
+        break
+      }
+    }
+    if(zero.length === 2) {break}
+  }
+  if (!zero || zero.length === 1) {return arr}
+  for(let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (i === zero[1]) {
+        arr[i][j] = 0
+      } else if (j === zero[0]) {
+        arr[i][j] = 0
+      }
+    }
+  }
+  return arr
 }
 
 if(typeof exports !== 'undefined') {
--- test/arrays&strings/07_zeroMatrix_test.js
+++ test/arrays&strings/07_zeroMatrix_test.js
@@ -6,11 +6,40 @@ let zeroMatrix = require('../../problems/arrays&strings/07_zeroMatrix.js').zeroM
 let arr
 
 describe('zeroMatrix', function() {
-  
-  xit('returns an array', function() {
+
+  it('returns an array', function() {
     arr = []
     expect(typeof(zeroMatrix(arr)) == 'object').equal(true)
   })
-  
-  
+
+  it('returns array untouched if no 0 is found', () => {
+    arr = [[1,2], [4,5]]
+    let newArr = zeroMatrix(arr)
+    expect(newArr[0][0] === 1 && newArr[0][1] === 2 && newArr[1][0] === 4, newArr[1][1] === 5).equal(true)
+  })
+
+  describe('0 is found', () => {
+    it('row contains only zeroes', () => {
+      arr = [[1,2,3], [4,0,5], [6,7,8], [9,10,11]]
+      let newArr = zeroMatrix(arr)
+      expect(newArr[1][0] === 0 && newArr[1][2] === 0).equal(true)
+    })
+
+    it('column contains only zeroes', () => {
+      arr = [[1,2,3], [4,0,5], [6,7,8], [9,10,11]]
+      let newArr = zeroMatrix(arr)
+      expect(newArr[0][1] === 0 && newArr[1][1] === 0 && newArr[2][1] === 0).equal(true)
+    })
+
+    it('works with non rectanglur matrixes', () => {
+      arr = [[1,2,3], [4,0,5], [6,7,8], [9,10,11]]
+      arr[1].push(5)
+      arr[1].push(9)
+      arr[3].push(11)
+      let newArr = zeroMatrix(arr)
+      expect(newArr[1][0] === 0 && newArr[1][2] === 0).equal(true)
+    })
+  })
+
+
 })
